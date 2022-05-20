75b4517afbc74c2393b880d319c05aa25d3c866c
Sanika222 <56600716+Sanika222@users.noreply.github.com>
2022-05-20 15:11:34 +0530

    Add files via upload

---
+++ Hello/src/TestAOP.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sky.aop.ApplicationService;
+
+public class TestAOP {
+	public static void main(String[] args) {
+		ApplicationContext beanFactory =new ClassPathXmlApplicationContext("file.xml");
+		ApplicationService annoservice =(ApplicationService)beanFactory.getBean(ApplicationService.class);
+		System.out.println(annoservice);
+		annoservice.processApplication();
+
+	}
+
+}
